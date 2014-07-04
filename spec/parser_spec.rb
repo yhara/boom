e13d42d09da661f7ca0a9633f5747051d6c28ec1
Yutaka HARA <yutaka.hara+github@gmail.com>
2014-07-04 13:44:22 +0900

    Rearrange test cases

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,31 +5,42 @@ describe Parser do
     Parser.parse(str)
   end
 
-  it 'literal' do
-    src = "1"
-    expect(parse(src)).to eq(
-      [:lit, "Int", 1]
-    )
-  end
+  context 'value' do
+    it 'anonfunc' do
+      src = "fn(x){ 1 }"
+      expect(parse(src)).to eq(
+        [:abs, "x", nil, [:lit, "Int", 1]]
+      )
+    end
 
-  it 'anonfunc' do
-    src = "fn(x){ 1 }"
-    expect(parse(src)).to eq(
-      [:abs, "x", nil, [:lit, "Int", 1]]
-    )
-  end
+    it 'funcall' do
+      src = "print(1)"
+      expect(parse(src)).to eq(
+        [:app, [:var, "print"], [:lit, "Int", 1]]
+      )
+    end
 
-  it 'varref' do
-    src = "print"
-    expect(parse(src)).to eq(
-      [:var, "print"]
-    )
-  end
+    it 'varref' do
+      src = "print"
+      expect(parse(src)).to eq(
+        [:var, "print"]
+      )
+    end
 
-  it 'funcall' do
-    src = "print(1)"
-    expect(parse(src)).to eq(
-      [:app, [:var, "print"], [:lit, "Int", 1]]
-    )
+    context 'literal' do
+      it 'number' do
+        src = "1"
+        expect(parse(src)).to eq(
+          [:lit, "Int", 1]
+        )
+      end
+
+      it 'string' do
+        src = '"a"'
+        expect(parse(src)).to eq(
+          [:lit, "String", "a"]
+        )
+      end
+    end
   end
 end
