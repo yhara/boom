c73eaf860b3377264368bd164aa75653da2c9849
Yutaka HARA <yutaka.hara+github@gmail.com>
2014-08-25 12:03:00 +0900

    Fix failing test
    Removed assetion of `subst`

--- spec/inferer_spec.rb
+++ spec/inferer_spec.rb
@@ -14,45 +14,34 @@ module Boom
       end
 
       def infer(expr, env={})
-        TypeInference.infer(expr, env)
+        subst, type = *TypeInference.infer(expr, env)
+        return type
       end
 
       it 'lit' do
         expr = [:lit, "Int", 7]
-        expect(infer(expr)).to eq(
-          [{}, TyRaw["Int"]]
-        )
+        expect(infer(expr)).to eq(TyRaw["Int"])
       end
 
       it 'abs' do
         expr = [:abs, "x", nil, [:var, "x"]]
-        expect(infer(expr)).to eq(
-          [{}, TyFun[TyVar[1], TyVar[1]]]
-        )
+        expect(infer(expr)).to eq(TyFun[TyVar[1], TyVar[1]])
       end
 
       it 'app' do
         expr = [:app, [:abs, "x", nil, [:var, "x"]], [:lit, "Int", 7]]
-        expect(infer(expr)).to eq(
-          [{1 => TyRaw["Int"], 2 => TyRaw["Int"]},
-            TyRaw["Int"]]
-        )
+        expect(infer(expr)).to eq(TyRaw["Int"])
       end
 
       it 'let value' do
         expr = [:let, "x", [:lit, "Int", 7], [:lit, "Int", 8]]
-        expect(infer(expr)).to eq(
-          [{}, TyRaw["Int"]]
-        )
+        expect(infer(expr)).to eq(TyRaw["Int"])
       end
 
       it 'let func' do
         expr = [:let, "f", [:abs, "x", nil, [:var, "x"]],
                  [:app, [:var, "f"], [:lit, "Int", 7]]]
-        expect(infer(expr)).to eq(
-          [{2 => TyRaw["Int"], 3 => TyRaw["Int"]},
-            TyRaw["Int"]]
-        )
+        expect(infer(expr)).to eq(TyRaw["Int"])
       end
 
       it 'seq' do
@@ -61,9 +50,7 @@ module Boom
                  [:seq, [:app, [:var, "f"], [:lit, "String", "hi"]],
                         [:var, "f"]]]]
                        
-        expect(infer(expr)).to eq(
-          [{}, TyFun[TyVar[6], TyRaw["Int"]]]
-        )
+        expect(infer(expr)).to eq(TyFun[TyVar[6], TyRaw["Int"]])
       end
 
       it 'with predefined funcs' do
@@ -73,17 +60,12 @@ module Boom
         env = {
           "succ" => ty_int_int,
         }
-        expect(TypeInference.infer(expr, env)).to eq(
-          [{2 => ty_int_int, 3 => ty_int_int},
-           ty_int_int]
-        )
+        expect(infer(expr, env)).to eq(ty_int_int)
       end
 
       it 'abs with type annotation' do
         expr = [:abs, "x", "Int", [:var, "x"]]
-        expect(infer(expr)).to eq(
-          [{}, TyFun[TyRaw["Int"], TyRaw["Int"]]]
-        )
+        expect(infer(expr)).to eq(TyFun[TyRaw["Int"], TyRaw["Int"]])
       end
 
       it 'defclass' do
@@ -91,9 +73,7 @@ module Boom
           class A; end
           A
         ")
-        expect(infer(expr)).to eq(
-          [{}, TyRaw["Class"]]
-        )
+        expect(infer(expr)).to eq(TyRaw["Class"])
       end
 
       it 'instance' do
@@ -101,9 +81,7 @@ module Boom
           class A; end
           A.new()
         ")
-        expect(infer(expr)).to eq(
-          [{1 => TyRaw["A"]}, TyRaw["A"]]
-        )
+        expect(infer(expr)).to eq(TyRaw["A"])
       end
     end
   end
